a04e27f122e81aaf4e7a98456dea201fa90d8dcb
Yaoqi Guo <yaoqi.guo121@gmail.com>
2018-10-04 15:04:19 -0400

    Deleted states with no universities in Map Tab

--- app/ui.r
+++ app/ui.r
@@ -22,11 +22,18 @@ require(data.table)
 library(dplyr)
 
 # Choices for drop-downs
-location = c("-----","AK","AL","AR","AS","AZ","CA","CO","CT","DC","DE"
-             ,"FL","FM","GA","GU","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD"
-             ,"ME","MH","MI","MN","MO","MP","MS","MT","NC","ND","NE","NH","NJ","NM","NV"
+# Delete states without universities: AS, FM, GU, MH, MP, PR, PW
+# location = c("-----","AK","AL","AR","AS","AZ","CA","CO","CT","DC","DE"
+#              ,"FL","FM","GA","GU","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD"
+#              ,"ME","MH","MI","MN","MO","MP","MS","MT","NC","ND","NE","NH","NJ","NM","NV"
+#              ,"NY","OH","OK","OR"
+#              ,"PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT"
+#              ,"WA","WI","WV","WY")
+location = c("-----","AK","AL","AR","AZ","CA","CO","CT","DC","DE"
+             ,"FL","GA","HI","IA","ID","IL","IN","KS","KY","LA","MA","MD"
+             ,"ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV"
              ,"NY","OH","OK","OR"
-             ,"PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT"
+             ,"PA","RI","SC","SD","TN","TX","UT","VA","VI","VT"
              ,"WA","WI","WV","WY")
 
 Major = c("-----",
